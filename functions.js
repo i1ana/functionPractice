c25c513cc4bb823b1da8e89c55bc44dfd5c5740a
Ilana <bachwasbaroque@gmail.com>
2016-01-22 14:09:22 -0700

    yo

---
+++ functions.js
@@ -0,0 +1,23 @@
+var i = 3; 
+
+function tripleFive(){
+	for(i = 0; i <= 3; i++){
+		console.log("Five!")
+	}
+}
+
+function excite (){
+
+}
+
+function tiny(i){
+	if(i > 0 && i < 1){
+		return true;
+	} else {
+		return false; 
+	}
+}
+
+// function getSeconds(wx$yz){
+
+// }
